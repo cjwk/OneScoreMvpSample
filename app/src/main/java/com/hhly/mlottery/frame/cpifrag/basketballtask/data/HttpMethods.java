e2ff34dc0a30011b9142057e60c2cc2945549527
wangg <wangg@13322.com>
2017-04-08 17:20:44 +0800

    优化websocket

--- app/src/main/java/com/hhly/mlottery/frame/cpifrag/basketballtask/data/HttpMethods.java
+++ app/src/main/java/com/hhly/mlottery/frame/cpifrag/basketballtask/data/HttpMethods.java
@@ -14,8 +14,8 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+
 
 /**
  * @author: Wangg
--- app/src/main/java/com/hhly/mlottery/mvp/BasePresenter.java
+++ app/src/main/java/com/hhly/mlottery/mvp/BasePresenter.java
@@ -3,6 +3,12 @@ package com.hhly.mlottery.mvp;
 import com.hhly.mlottery.MyApp;
 import com.hhly.mlottery.data.DataManager;
 
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+
 /**
  * 描    述：Presenter 基类
  * 作    者：mady@13322.com
@@ -14,6 +20,8 @@ public abstract class BasePresenter<V extends IView> implements IPresenter<V> {
     protected V mView; //子类presenter可以接调用
 
     protected DataManager mDataManager;
+
+    CompositeSubscription mCompositeSubscription;
 
     private BasePresenter() {
 
@@ -37,4 +45,28 @@ public abstract class BasePresenter<V extends IView> implements IPresenter<V> {
     public void detachView() {
         //Rx取消注册
     }
+
+
+    /**
+     * rxJava取消注册，以免内存泄露
+     */
+    protected void onUnSubscribe() {
+        if (mCompositeSubscription != null && mCompositeSubscription.hasSubscriptions()) {
+            mCompositeSubscription.unsubscribe();
+        }
+    }
+
+    public <T> void addSubscription(Observable<T> observable, Subscriber<T> subscriber) {
+        if (mCompositeSubscription == null) {
+            mCompositeSubscription = new CompositeSubscription();
+        }
+        Subscription subscription = observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+        mCompositeSubscription.add(subscription);
+    }
+
+
 }
